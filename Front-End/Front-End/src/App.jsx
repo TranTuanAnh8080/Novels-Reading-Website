b2b1e967f59ef7de118ea6b9714255257a4ca9c0
vnhatnam <nhatnam18112004@gmail.com>
2025-10-08 19:43:16 +0700

    Tích hợp ChatBot vào App.jsx

--- Front-End/Front-End/src/App.jsx
+++ Front-End/Front-End/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AppRouter from '../src/routes'
 import { RouterProvider } from 'react-router-dom';
 import ScrollToTopButton from './pages/ScrollToTopButton';
+import ChatBot from './components/SharedComponents/ChatBot';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
     
       <RouterProvider router={AppRouter} />
       <ScrollToTopButton />
+      <ChatBot />
     </>
   )
 }
