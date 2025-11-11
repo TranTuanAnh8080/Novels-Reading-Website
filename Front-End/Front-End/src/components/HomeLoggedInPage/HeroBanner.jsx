e30f3caf7676a81d368d62de1958e8e789001046
TranTuanAnh8080 <trantuananhhh8088@gmail.com>
2025-11-11 17:09:41 +0700

    fixed completed

--- Front-End/Front-End/src/components/HomeLoggedInPage/HeroBanner.jsx
+++ Front-End/Front-End/src/components/HomeLoggedInPage/HeroBanner.jsx
@@ -6,7 +6,7 @@ function HeroBanner() {
       <img
         src="https://i.pinimg.com/1200x/7c/c6/29/7cc629470a4fb1e07ec0c7c16a203a16.jpg"
         alt="Banner"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover brightness-100 contrast-100 saturate-200"
       />
     </div>
   );
