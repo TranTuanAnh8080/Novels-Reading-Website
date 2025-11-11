95361a6392d67adbb376cb168ba9db2e3e0dac7b
TranTuanAnh8080 <trantuananhhh8088@gmail.com>
2025-11-11 16:58:33 +0700

    .

--- Front-End/Front-End/src/components/HomeLoggedInPage/HeroBanner.jsx
+++ Front-End/Front-End/src/components/HomeLoggedInPage/HeroBanner.jsx
@@ -6,7 +6,7 @@ function HeroBanner() {
       <img
         src="https://i.pinimg.com/1200x/7c/c6/29/7cc629470a4fb1e07ec0c7c16a203a16.jpg"
         alt="Banner"
-        className="w-full h-full object-cover contrast-125 brightness-95 saturat-125 "
+        className="w-full h-full object-cover contrast-150 brightness-90 saturate-150 "
       />
     </div>
   );
